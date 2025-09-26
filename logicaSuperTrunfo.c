fb848d7bbbc61a044d4b750755160bbe5695ab48
laradiego41961-oss <laradiego41961@gmail.com>
2025-09-26 17:56:53 +0000

    CODIGO FINALIZADO E VERIFICADO COM SUCESSO!

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-
-// Desafio#include <stdio.h>
 #include <string.h>
 
 // Estrutura para armazenar os dados de uma carta
@@ -65,8 +63,6 @@ int main() {
 
     // ==========================
     // Comparação de atributo
-    // Troque aqui o atributo para comparar outro
-    // Exemplo: "populacao", "area", "pib", "densidade", "pibPerCapita"
     // ==========================
     printf("== Comparação de cartas (Atributo: População) ==\n\n");
 
@@ -80,46 +76,6 @@ int main() {
     } else {
         printf("Resultado: Empate!\n");
     }
-
-    return 0;
-} Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
-
-int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
-
-    
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
-
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
-
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
-
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
-
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
 
     return 0;
 }
